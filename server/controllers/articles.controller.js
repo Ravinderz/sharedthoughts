2c9b17628a4db1c1b07ccbf474e624acf8a5af0c
Ravinder Singh <ravinderz@outlook.com>
2017-11-21 23:01:19 +0530

    user apis complete

---
+++ server/controllers/articles.controller.js
@@ -0,0 +1,8 @@
+const user = require('../models/article');
+const mongojs = require('mongojs');
+const mongoose = require('mongoose');
+const config = require('../../config');
+
+exports.write_article = function(req,res){
+    
+}
